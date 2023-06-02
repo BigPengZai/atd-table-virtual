ba6ab5f5a6c29bf47ba7653f427fc651a52656d7
pengppeng <1163871686@qq.com>
2023-06-02 09:48:42 +0800

    doc:展示滚动效果

--- src/components/ATablePer.vue
+++ src/components/ATablePer.vue
@@ -5,9 +5,26 @@
   <!-- VirtualTableScroll -->
   <div class="mockContainer">
     <a-table
-      v-virtual-table-scroll
+      v-virtual-table-scroll="loadSourceData"
       :columns="columns"
       :data-source="dataList.slice(start, over)"
+      bordered
+      :pagination="false"
+      :scroll="{ x: 1080, y: scrollHeight }"
+    >
+      <template #address="{ text }">
+        <div>
+          {{ text }}
+        </div>
+      </template>
+    </a-table>
+  </div>
+
+  <div class="normal-table">
+    <a-button type="primary">normal-table</a-button>
+    <a-table
+      :columns="columns2"
+      :data-source="dataList2"
       bordered
       :pagination="false"
       :scroll="{ x: 1080, y: scrollHeight }"
@@ -30,12 +47,13 @@ const { start, over } = inject("dataListOptions");
 // 定义table的高度
 let scrollHeight = 300;
 // mock data 加载500行
-let totalRows = 500;
+let totalRows = 300;
 // 加载 30列
-let totalColumns = 29;
+let totalColumns = 299;
 
 // init 行数据
 let dataList = ref([]);
+let dataList2 = ref([]);
 // init 列数据
 let columns = ref([
   {
@@ -45,12 +63,25 @@ let columns = ref([
   },
 ]);
 
+let columns2 = ref([
+  {
+    title: "Name",
+    dataIndex: "name",
+    width: 133,
+  },
+]);
+
+let totalRows2 = 300;
+// 加载 30列
+let totalColumns2 = 299;
+
+let now = null;
+
 // axios mock 加载数据
 //  v-virtual-table-scroll="loadSourceData" 这里绑定的loadSourceData,会先于 该组件的onMounted 执行
 const loadSourceData = () => {
   console.log("loadSourceData");
 
-  const now = Date.now();
   setTimeout(() => {
     for (let index = 1; index <= totalRows; index++) {
       dataList.value.push({
@@ -87,12 +118,52 @@ const _loadColums = () => {
 
 onMounted(() => {
   console.log("onMounted");
-  loadSourceData();
+  // loadSourceData();
+  now = Date.now();
   _loadColums();
+
+  loadSourceData2();
 });
+
+const loadSourceData2 = () => {
+  console.log("loadSourceData2");
+
+  setTimeout(() => {
+    for (let index = 1; index <= totalRows2; index++) {
+      dataList2.value.push({
+        key: index + 1,
+        name: "petyon" + index,
+        money: "￥120,000.00",
+        address: "Sidney No." + index,
+        operation: "aaaaa" + index,
+      });
+    }
+    console.log("normal-mock fetch数据请求完成:", Date.now() - now - 200);
+    setTimeout(() => {
+      console.log(
+        `normal-table 渲染${totalRows2}行,${totalColumns2 + 1}列时间:`,
+        Date.now() - now - 400
+      );
+    }, 0);
+  }, 400);
+
+  for (let index = 1; index <= totalColumns2; index++) {
+    columns2.value.push({
+      title: "address" + index,
+      dataIndex: "address",
+      width: 133,
+      slots: {
+        customRender: "address",
+      },
+    });
+  }
+};
 </script>
 
 <style scoped>
+.mockContainer {
+  width: 100%;
+}
 .normal-table {
   margin-top: 50px;
 }
