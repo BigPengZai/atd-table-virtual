ee66f9217de66f71db7b9a09a8f29031c75d6603
yushengyuan <yusy@zz-med.com>
2024-04-07 14:30:18 +0800

    feat: add binary search

---
+++ src/utils/binarySearch.js
@@ -0,0 +1,21 @@
+// 二分查找
+export const binarySearch = (value) => {
+  let start = 0;
+  let end = Object.keys(positions).length - 1;
+  let tempIndex = null;
+  while (start <= end) {
+    const midIndex = Math.floor((start + end));
+    const midValue = positions[midIndex].bottom;
+    if (midValue === value) {
+      return midIndex + 1;
+    } else if (midValue < value) {
+      start = midIndex + 1;
+    } else if (midValue > value) {
+      if (tempIndex === null || tempIndex > midIndex) {
+        tempIndex = midIndex;
+      }
+      end = end - 1;
+    }
+  }
+  return tempIndex;
+};
